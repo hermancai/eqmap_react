8fd7b394b9454e133aa1c01b870aa461adeb8291
Herman Cai <hermancai3@gmail.com>
2022-08-13 05:20:43 -0700

    Refactor render map and markers

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,7 +1,6 @@
 import { FC, useState, useEffect, useCallback, useRef } from "react";
 import { USGSReturnedObject } from "../models/USGSDataType";
 import { Marker, GoogleMap } from "@react-google-maps/api";
-import useLoadWindowWithGoogle from "../hooks/useLoadWindowWithGoogle";
 
 type MarkerDetails = {
   position: google.maps.LatLng | google.maps.LatLngLiteral;
@@ -14,84 +13,74 @@ type MarkerDetails = {
 const Results: FC<{ data: USGSReturnedObject | null }> = ({ data }) => {
   const sectionRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const { isLoaded } = useLoadWindowWithGoogle();
-
   const [markerList, setMarkerList] = useState<MarkerDetails[]>([]);
-  const [bounds, setBounds] = useState<google.maps.LatLngBounds | null>(null);
 
-  const buildMarkerList = useCallback(
-    (data: USGSReturnedObject): MarkerDetails[] => {
-      const newBounds = new google.maps.LatLngBounds(data.center);
-      const newMarkerList = data.features.map((entry): MarkerDetails => {
-        const position = {
-          lat: entry.geometry.coordinates[1],
-          lng: entry.geometry.coordinates[0],
-        };
+  useEffect(() => {
+    if (data === null || map === null) return setMarkerList([]);
 
-        const redIcon: google.maps.Symbol = {
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: entry.properties.mag * 5,
-          fillColor: "red",
-          fillOpacity: 0.25,
-          strokeColor: "white",
-          strokeWeight: 0.5,
-        };
+    const bounds = new google.maps.LatLngBounds(data.center);
+    const newMarkerList = data.features.map((entry): MarkerDetails => {
+      const position = {
+        lat: entry.geometry.coordinates[1],
+        lng: entry.geometry.coordinates[0],
+      };
 
-        const infoWindow = new google.maps.InfoWindow({
-          content: `<p>${entry.properties.place}<br>Magnitude: ${
-            entry.properties.mag
-          }<br>${new Date(entry.properties.time).toLocaleString()}</p>`,
-          position: position,
-          pixelOffset: new google.maps.Size(0, entry.properties.mag * -5),
-        });
+      const redIcon: google.maps.Symbol = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: entry.properties.mag * 5,
+        fillColor: "red",
+        fillOpacity: 0.25,
+        strokeColor: "white",
+        strokeWeight: 0.5,
+      };
 
-        newBounds.extend(position);
-
-        return {
-          position: position,
-          icon: redIcon,
-          time: entry.properties.time,
-          onMouseOver: () => {
-            infoWindow.open({ map: map });
-          },
-          onMouseOut: () => infoWindow.close(),
-        };
+      const infoWindow = new google.maps.InfoWindow({
+        content: `<p>${entry.properties.place}<br>Magnitude: ${
+          entry.properties.mag
+        }<br>${new Date(entry.properties.time).toLocaleString()}</p>`,
+        position: position,
+        pixelOffset: new google.maps.Size(0, entry.properties.mag * -5),
       });
-      newMarkerList.push({
-        position: data.center,
-        time: new Date().getTime(),
-        onMouseOut: () => null,
-        onMouseOver: () => null,
-      });
-      setBounds(newBounds);
-      return newMarkerList;
-    },
-    [map]
-  );
 
-  const onMapLoad = useCallback(
-    (map: google.maps.Map): void => {
-      if (data === null || bounds === null) return;
-      map.fitBounds(bounds);
-      setMap(map);
-    },
-    [data, bounds]
-  );
+      bounds.extend(position);
+
+      return {
+        position: position,
+        icon: redIcon,
+        time: entry.properties.time,
+        onMouseOver: () => {
+          infoWindow.open({ map: map });
+        },
+        onMouseOut: () => infoWindow.close(),
+      };
+    });
+
+    newMarkerList.push({
+      position: data.center,
+      time: new Date().getTime(),
+      onMouseOut: () => null,
+      onMouseOver: () => null,
+    });
+
+    setMarkerList(newMarkerList);
+    if (newMarkerList.length > 1) map.fitBounds(bounds);
+
+    if (sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [data, map]);
+
+  const onMapLoad = useCallback((map: google.maps.Map): void => {
+    setMap(map);
+  }, []);
 
   const onMapUnmount = useCallback((): void => {
     setMarkerList([]);
     setMap(null);
   }, []);
 
-  useEffect((): void => {
-    if (data === null || !isLoaded) return;
-    setMarkerList(buildMarkerList(data));
-    if (sectionRef.current) {
-      sectionRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [data, buildMarkerList, isLoaded]);
+  if (data === null) return <></>;
 
-  if (data === null || !isLoaded) return <></>;
   return (
     <div
       ref={sectionRef}
@@ -110,11 +99,14 @@ const Results: FC<{ data: USGSReturnedObject | null }> = ({ data }) => {
           height: "500px",
         }}
         center={data.center}
-        zoom={8}
+        zoom={9}
       >
         {markerList.map((entry) => (
           <Marker
-            onMouseOver={() => entry.onMouseOver()}
+            onMouseOver={() => {
+              console.log("hover");
+              entry.onMouseOver();
+            }}
             onMouseOut={() => entry.onMouseOut()}
             position={entry.position}
             icon={entry.icon}
